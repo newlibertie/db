29c3bd55d087f23fd95257c26701e4ec68f77c77
vivekpathak <vpathak+github@orgmeta.com>
2019-06-12 23:59:08 -0400

    more compact representation
      computed fields need not be stored
      increase size of cryptographic fields
      standardize naming to c style as myql suggests

--- create_schema.sql
+++ create_schema.sql
@@ -6,7 +6,7 @@ USE nldb;
 
 CREATE TABLE IF NOT EXISTS users (
 
-    id CHAR(36) PRIMARY KEY COMMENT 'uuid generated at creation time',
+    id CHAR(36) PRIMARY KEY COMMENT 'uuid (8-4-4-12) generated at creation time',
 
     url VARCHAR(8192) NOT NULL COMMENT 'url pointing to a user page',
 
@@ -22,79 +22,70 @@ CREATE TABLE IF NOT EXISTS users (
 
 
 CREATE TABLE IF NOT EXISTS polls (
-	id VARCHAR(32) NOT NULL
-		COMMENT 'auto generated at creation time only, never updated',
-	tittle VARCHAR(64) NOT NULL
-		COMMENT 'provided at the creation time and can be updated, never empty',
-	tags JSON, #| array of tags
-	#parameters	| h is computed, not stored
-	largePrimeP VARCHAR(1024) NOT NULL
-		COMMENT 'populated at the creation time only, never updated',
-	generatorG VARCHAR(1024) NOT NULL
-		COMMENT 'populated at the creation time only, never updated',
-	privateKeyS VARCHAR(1024) NOT NULL
-		COMMENT 'populated at the creation time only, never updated',
-	#creator
-	creatorId VARCHAR(32) NOT NULL
-		COMMENT 'populated at creation time with users.id, never updated',
-	creatorURL VARCHAR(255) NOT NULL
-		COMMENT 'populated at creation time with users.url, never updated',
 
-	#scope
-	openingTS TIMESTAMP NULL
-		COMMENT 'user provided input and smaller than closingTS if present, can be updated as long as status allows',
-	closingTS TIMESTAMP NULL
-		COMMENT 'user provided input and bigger than openingTS if present, can be updated as long as status allows',
+    id VARCHAR(36) PRIMARY KEY COMMENT 'uuid (8-4-4-12) generated at creation time',
 
-	#houseKeeping
-	creationTS TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		COMMENT 'housekeeping field, populated only at creation time, never updated',
-	lastModificationTS TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-		COMMENT 'housekeeping field, populated at creation time and each update time',
+    title VARCHAR(64) NOT NULL,
 
-	#state fields
-	publishedTS TIMESTAMP NULL
-		COMMENT 'can be updated only if it was NULL before and its value is not more than openingTS',
-	openTS TIMESTAMP NULL
-		COMMENT 'can be updated only if it was NULL before and its value is not less than publishedTS',
-	closedTS TIMESTAMP NULL
-		COMMENT 'can be updated only if it was NULL before and its value is not less than openTS',
+    tags VARCHAR(65535) NULL  COMMENT "JSON array of tags",
 
-	status ENUM('created', 'published', 'open', 'closed') NOT NULL,
-	type ENUM('simple', 'priority', 'order', 'ranking', 'weights') NOT NULL
-		COMMENT 'an interpreter required per type value to interpret the json',
-	choices JSON
-		COMMENT 'can be updated as long as status allows and content has to be validated by an interpretor corresponding to type',
+    -- cryptographic parameters for this poll
 
-	#estimate fields
-	likely BIGINT
-		COMMENT 'a vote count estimate, can be updated as allowed by status',
-	maximum BIGINT
-		COMMENT 'a vote count estimate, can be updated as allowed by status'
+    large_prime_p VARCHAR(65535) NOT NULL,
+
+    generator_g  VARCHAR(65535) NOT NULL,
+
+    private_key_s VARCHAR(65535) NOT NULL,
+
+
+    -- creator
+    creator_id  VARCHAR(36) NOT NULL COMMENT 'populated at creation time with users.id, never updated',
+
+
+    -- operational parameters of this poll
+    opening_ts TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    closing_ts TIMESTAMP NOT NULL,
+
+    -- housekeeping
+
+    creation_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    last_modification_ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+
+
+    poll_type ENUM(
+              'simple',             -- one choice allowed, choice with maximum votes wins
+              'multiple_choice',    -- select as many as yoo like,  choice with maximum votes wins
+              'instant_runoff'      -- select as many as you like in priority order,  instant runoff
+              ) NOT NULL
+        COMMENT 'an interpreter required per type value to interpret the json in the choices field',
+
+    poll_choices  VARCHAR(65535)
 );
 
 CREATE TABLE IF NOT EXISTS ballots (
-	pollId VARCHAR(32) NOT NULL
-		COMMENT 'reference to poll.id, populated only on creation, never updated',
-	voterURL VARCHAR(255) NOT NULL
-		COMMENT 'populated only on creation, never updated',
-	value JSON  #string json( eg: array of (T, C, D) )
-		COMMENT 'value validated by an interpretor based on poll.type, populated only on creation, never updated',
-	creationTS TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		COMMENT 'populated only on creation, never updated'
+    pollId VARCHAR(32) NOT NULL
+        COMMENT 'reference to poll.id, populated only on creation, never updated',
+    voterURL VARCHAR(255) NOT NULL
+        COMMENT 'populated only on creation, never updated',
+    value JSON  #string json( eg: array of (T, C, D) )
+        COMMENT 'value validated by an interpretor based on poll.type, populated only on creation, never updated',
+    creationTS TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        COMMENT 'populated only on creation, never updated'
 );
 
 
 
 CREATE TABLE IF NOT EXISTS disqualifications (
-	pollId VARCHAR(32) NOT NULL
-		COMMENT 'refersence to poll.id, populated on creation, never updated',
-	voterId VARCHAR(255) NOT NULL,
-	tsstart TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-		COMMENT 'populated on creation, never updated',
-	tsend TIMESTAMP NULL
-		COMMENT 'updated only if NULL, not populated on creation',
-	reasonstart VARCHAR(255)
-		COMMENT 'populated only on creation, never updated', 
-		COMMENT 'updated only if NULL, not populated on creation'
+    pollId VARCHAR(32) NOT NULL
+        COMMENT 'refersence to poll.id, populated on creation, never updated',
+    voterId VARCHAR(255) NOT NULL,
+    tsstart TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        COMMENT 'populated on creation, never updated',
+    tsend TIMESTAMP NULL
+        COMMENT 'updated only if NULL, not populated on creation',
+    reasonstart VARCHAR(255)
+        COMMENT 'populated only on creation, never updated',
+        COMMENT 'updated only if NULL, not populated on creation'
 );
